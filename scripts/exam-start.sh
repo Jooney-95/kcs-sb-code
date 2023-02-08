a102ea3a480a0af5f8f7172656a239523693e148
root <root@ip-10-10-1-58.ap-northeast-2.compute.internal>
2023-02-08 09:06:42 +0000

    fix: start.sh with stop.sh fix

--- scripts/exam-start.sh
+++ scripts/exam-start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo /home/ec2-user/tomcat/bin/startup.sh
+sudo /home/ec2-user/tomcat/bin/startup.sh 1> /dev/null 2>&1 &
+echo $! > pid.file
--- scripts/exam-stop.sh
+++ scripts/exam-stop.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+filename=pid.file # pid.file에 있는 값을 filename 변수에 넣어준다.
+pid=`cat $filename` # filename 변수의 값을 출력해서 pid에 저장한다.
+kill $pid # 프로세스 삭제
+cp /dev/null filename # filename 변수를 초기화
 sudo /home/ec2-user/tomcat/bin/shutdown.sh
